a737c5b4bbd5dfc6a21d240d0fba3b5443db50d4
Rph <rphsoftware@users.noreply.github.com>
2022-10-03 14:35:43 +0200

    Serenity-compatibility

--- src/headers.h
+++ src/headers.h
@@ -56,8 +56,10 @@ extern DECLSPEC int SDLCALL SDL_GetGamma(float *red, float *green, float *blue);
 #define textdomain(x) while(false)
 #endif
 
-#if !defined(OpenBSD) && !defined(FreeBSD) && !defined(__APPLE__) && !defined(__serenity__)
+#if !defined(OpenBSD) && !defined(FreeBSD) && !defined(__APPLE__)
 static inline void strlcat(char *dest, const char *src, size_t n) { strncat(dest, src, n - 1); }
+#endif
+#if !defined(OpenBSD) && !defined(FreeBSD) && !defined(__APPLE__) && !defined(__serenity__)
 static inline void strlcpy(char *dest, const char *src, size_t n) { strncpy(dest, src, n); dest[n - 1] = 0; }
 #endif
 
